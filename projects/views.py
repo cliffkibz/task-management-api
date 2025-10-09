e86f43208c8b8f31e734858059f57dbd6c4e709c
Wycliffe cheruiyot <53683890+cliffkibz@users.noreply.github.com>
2025-10-09 19:23:59 +0300

    Added a urls py to load the paths

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Project, Task
+from .serializers import ProjectSerializer, TaskSerializer
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
