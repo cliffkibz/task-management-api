2424aee414b0be30e42da0e09ee74745c86fcfaf
Wycliffe cheruiyot <53683890+cliffkibz@users.noreply.github.com>
2025-10-18 13:42:35 +0300

    configured jwt in settings and added jwt urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -18,9 +18,16 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from django.views.generic import RedirectView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
     path('api/', include('projects.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
