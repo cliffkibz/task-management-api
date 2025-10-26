5061ee24e2a61f841ad2c0b08121579ae95b773a
Wycliffe cheruiyot <53683890+cliffkibz@users.noreply.github.com>
2025-10-26 14:36:38 +0300

    updated the admin to show the projects and tasks recorded

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Project, Task
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at')
+    list_filter = ('created_at', 'user')
+    search_fields = ('title', 'description', 'user__username')
+    date_hierarchy = 'created_at'
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'status', 'priority', 'due_date', 'created_at')
+    list_filter = ('status', 'priority', 'due_date', 'created_at', 'project')
+    search_fields = ('title', 'description', 'project__title')
+    date_hierarchy = 'created_at'
